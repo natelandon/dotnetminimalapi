48ab44abe93a398f128fe3c27cb764598cdb3c0a
natelandon <njlandon@gmail.com>
2024-04-26 21:07:55 -0500

    InitialCommit

---
+++ Todo.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.DataProtection;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+    public string? Secret { get; set; }
+}
